d2082984eea5bea103507127e5c513c09983dabf
edson-onishi <edson@hotmail.com>
2020-09-16 14:58:05 -0300

    include theory

---
+++ conteudo/estruturaRepeticao.py
@@ -0,0 +1,27 @@
+for c in range(1,6):
+    print("Oi")
+    print("Final")
+print("FIM")    
+
+for c in range(0,6):\
+    print(c)
+print("FIM")    
+
+for c in range(6, 0, -1):
+    print(c)
+print("FIM")    
+
+for c in range(1, 7, -2):
+    print(c)
+print("FIM")  
+
+n = int(input("Digite um nuemro: "))
+for c in range(0, n+1):
+    print(c)
+print("FIM")    
+
+s = 0
+for c in range(0,4):
+    n = int(input('DIgite um valor: '))
+    s += n
+print("O somatorio de todos os valores foi {} ".format(s))    
